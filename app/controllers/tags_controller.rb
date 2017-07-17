fb30cd646b63edcd78c12d90857ce6beabe4adb7
Joshua Rudio <joshuarudio@gmail.com>
2017-07-16 20:31:48 -0700

    Build tag index and view

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,10 @@
 class TagsController < ApplicationController
+
+  def index
+    @tags = Tag.all
+  end
+
   def show
-    @tag = Tag.find(params[:id])
+    @tag = Tag.find(params[:format])
   end
 end
